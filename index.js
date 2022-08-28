e3f29b30255dd562c83f454c3e49cfa6395a5da5
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2022-08-28 15:20:23 -0300

    regexp

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const chalk = require('chalk');
+const fs = require('fs');
 
-console.log(chalk.blue('vamos começar!'));
-
-const paragrafo = 'Texto retornado por uma função';
-
-function texto(string) {
-  return string;
+const trataErro = (erro) => {
+  throw new Error(chalk.red(erro.code))
 }
 
-console.log(texto(paragrafo));
+// fs.readFile('./arquivos/texto1.md', 'utf-8', (erro, data)=> {
+  
+//   if(erro) {
+//     trataErro(erro)
+//   }
+
+//   console.log(chalk.green(data))
+// })
+
+// fs.promises.readFile('./arquivos/texto1.md', 'utf-8')
+// .then(data => console.log(chalk.green(data)))
+// .catch(erro => console.log(chalk.red(erro)))
+
+async function lerArquivo() {
+  try {
+    const file = await fs.promises.readFile('./arquivos/texto1.md', 'utf-8')
+    // console.log(file.match(/\[([^\]]*)\]\(https?:\/\/[^$#\s].[^\s]*\)/gm)) // Expressão usada no curso
+    console.log(file.match(/\d*\.\d*\.\d*-\d*/gm)) //Buscando CPF no meio do texto  
+  } catch (error) {
+    trataErro(error)
+  } finally {
+    console.log(chalk.yellow("Operação concluida !!!"))
+  }
+}
+
+lerArquivo()
+
